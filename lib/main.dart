bdebcdd8073e88dbdfb9300109504a399ff638ce
JoseCardoso-dev <jrcardoso541@gmail.com>
2023-08-02 21:16:34 -0300

    implemented StatefulWidget in display

--- lib/main.dart
+++ lib/main.dart
@@ -10,49 +10,120 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Tarefas'),
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-        body: Container(
+        home: Container(
           color: Colors.white,
-          child: Column(children: [
-            Stack(
+          child: ListView(
+            children: [
+              Task("Testeeeeeeee"),
+              Task("task_title"),
+              Task("Testeeeeeeee"),
+              Task("task_title"),
+              Task("Testeeeeeeee"),
+              Task("task_title asdasdasd asdasdasd asdasdas"),
+            ],
+          ),
+        ));
+  }
+}
+
+class Task extends StatefulWidget {
+  final String task_title;
+
+  const Task(this.task_title, {Key? key}) : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Column(
               children: [
                 Container(
-                  color: Colors.blue,
-                  width: 344,
-                  height: 140,
-                ),
-                Container(
                   color: Colors.white,
-                  width: 344,
                   height: 100,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
-                        color: Colors.grey,
-                        width: 80,
+                        color: Colors.black26,
+                        width: 72,
                         height: 100,
                       ),
-                      const Text('Compras do Mês'),
+                      Container(
+                        width: 140,
+                        child: Text(
+                          widget.task_title,
+                          style: const TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                            fontWeight: FontWeight.normal,
+                            decoration: TextDecoration.none,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
                       ElevatedButton(
-                          onPressed: () {},
-                          child: const Icon(Icons.arrow_drop_up)),
+                          onPressed: () {
+                            setState(() {
+                              nivel++;
+                            });
+                          },
+                          child: const Icon(Icons.arrow_drop_up))
                     ],
                   ),
-                )
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        //color: Colors.red,
+                        width: 190,
+                        child: LinearProgressIndicator(
+                          value: nivel / 10,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Nivel: $nivel",
+                        style: const TextStyle(
+                          fontSize: 13,
+                          color: Colors.white,
+                          fontWeight: FontWeight.normal,
+                          decoration: TextDecoration.none,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             )
-          ]),
+          ],
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
       ),
     );
   }
